9a70781e10965d993ed0d1300b78f4fecf3a2626
conbang <tuyen.tcd1@gmail.com>
2021-06-15 11:03:10 +0700

    fix register verify phone

--- src/app/user/board/label/label.component.spec.ts
+++ src/app/user/board/label/label.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { LabelComponent } from './label.component';
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+describe('LabelComponent', () => {
+  let component: LabelComponent;
+  let fixture: ComponentFixture<LabelComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ LabelComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(LabelComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,11 +5,14 @@ import { BoardComponent } from './board/board.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
 import {ShareModule} from '../share/share.module';
 import {UserRoutingModule} from './user-routing.module';
+import { ListComponent } from './board/list/list.component';
+import { CardComponent } from './board/card/card.component';
+import { LabelComponent } from './board/label/label.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent, BoardComponent, WorkspaceComponent],
+  declarations: [HomepageComponent, BoardComponent, WorkspaceComponent, ListComponent, CardComponent, LabelComponent],
     imports: [
         CommonModule,
         UserRoutingModule,
